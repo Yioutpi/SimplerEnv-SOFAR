b81834351a5c9058163c50d447ab86fa434b2393
Zhangwenyao1 <945117033@qq.com>
2025-02-22 19:53:40 +0800

    rm rt1 octo openvla

--- simpler_env/main_inference.py
+++ simpler_env/main_inference.py
@@ -4,15 +4,6 @@ import tensorflow as tf
 
 from simpler_env.evaluation.argparse import get_args
 from simpler_env.evaluation.maniskill2_evaluator import maniskill2_evaluator
-# from simpler_env.policies.octo.octo_server_model import OctoServerInference
-from simpler_env.policies.rt1.rt1_model import RT1Inference
-from simpler_env.policies.openvla.openvla_model import OpenVLAInference
-
-# try:
-#     from simpler_env.policies.octo.octo_model import OctoInference
-# except ImportError as e:
-#     print("Octo is not correctly imported.")
-#     print(e)
 
 
 if __name__ == "__main__":
@@ -30,21 +21,8 @@ if __name__ == "__main__":
         )
     print(f"**** {args.policy_model} ****")
     # policy model creation; update this if you are using a new policy model
-    if args.policy_model == "rt1":
-        assert args.ckpt_path is not None
-        model = RT1Inference(
-            saved_model_path=args.ckpt_path,
-            policy_setup=args.policy_setup,
-            action_scale=args.action_scale,
-        )
-    elif args.policy_model == "openvla":
-        assert args.ckpt_path is not None
-        model = OpenVLAInference(
-            saved_model_path=args.ckpt_path,
-            policy_setup=args.policy_setup,
-            action_scale=args.action_scale,
-        )
-    elif args.policy_model == "sofar":
+
+    if args.policy_model == "sofar":
         model = "sofar"
     elif args.policy_model == "sofar_widowx":
         model = "sofar_widowx"
